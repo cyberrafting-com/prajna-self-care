e1fe2ef61239707cfa45d26668b9af2e424679c9
Deovrat Bharadwaj <deovrat247@gmail.com>
2024-04-25 12:20:07 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -2,19 +2,19 @@
 
 $('#owl-carousel').owlCarousel({
     loop: true,
-    margin: 20,
+    margin: 10,
     responsiveClass: true,
     responsive: {
         0: {
-            items: 1,
-            nav: false
-        },
-        600: {
             items: 2,
             nav: false
         },
+        600: {
+            items: 3,
+            nav: false
+        },
         1000: {
-            items: 4,
+            items: 7,
             nav: false,
             loop: false
         }
